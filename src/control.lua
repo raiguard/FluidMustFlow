ffb02af00f84f9227ae8153d6d18c6661343c2e4
raiguard <caleb.heuer@gmail.com>
2021-12-30 22:02:37 -0700

    Listen for script_raised_revive, fix crash

--- src/control.lua
+++ src/control.lua
@@ -16,41 +16,43 @@ local function get_midpoint(pos_1, pos_2)
   }
 end
 
-event.register(
-  { defines.events.on_built_entity, defines.events.on_robot_built_entity, defines.events.script_raised_built },
-  function(e)
-    --- @type LuaEntity
-    local entity = e.entity or e.created_entity
-    if not entity or not entity.valid then
-      return
+event.register({
+  defines.events.on_built_entity,
+  defines.events.on_robot_built_entity,
+  defines.events.script_raised_built,
+  defines.events.script_raised_revive,
+}, function(e)
+  --- @type LuaEntity
+  local entity = e.entity or e.created_entity
+  if not entity or not entity.valid then
+    return
+  end
+
+  -- Straight ducts only have one fluidbox
+  for _, neighbour in pairs(entity.neighbours[1]) do
+    if entity.name == neighbour.name then
+      local direction = entity.direction
+      local force = entity.force
+      local last_user = entity.last_user
+      local name = entity.name == "duct-small" and "duct" or "duct-long"
+      local position = get_midpoint(entity.position, neighbour.position)
+      local surface = entity.surface
+
+      entity.destroy({ raise_destroy = true })
+      neighbour.destroy({ raise_destroy = true })
+
+      surface.create_entity({
+        name = name,
+        position = position,
+        direction = direction,
+        force = force,
+        player = last_user,
+        raise_built = true,
+        create_build_effect_smoke = false,
+      })
+
+      -- Only do one join per build
+      break
     end
-
-    -- Straight ducts only have one fluidbox
-    for _, neighbour in pairs(entity.neighbours[1]) do
-      if entity.name == neighbour.name then
-        local direction = entity.direction
-        local force = entity.force
-        local last_user = entity.last_user
-        local position = get_midpoint(entity.position, neighbour.position)
-        local surface = entity.surface
-
-        entity.destroy({ raise_destroy = true })
-        neighbour.destroy({ raise_destroy = true })
-
-        surface.create_entity({
-          name = entity.name == "duct-small" and "duct" or "duct-long",
-          position = position,
-          direction = direction,
-          force = force,
-          player = last_user,
-          raise_built = true,
-          create_build_effect_smoke = false,
-        })
-
-        -- Only do one join per build
-        break
-      end
-    end
-  end,
-  { { filter = "name", name = "duct-small" }, { filter = "name", name = "duct" } }
-)
+  end
+end, { { filter = "name", name = "duct-small" }, { filter = "name", name = "duct" } })
