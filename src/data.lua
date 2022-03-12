553082eec9a6a61f97621ca374cce2a42c0bcd01
raiguard <caleb.heuer@gmail.com>
2022-03-12 14:16:23 -0700

    Move ducts to the pipe subgroup with SE (fixes #5)

--- src/data.lua
+++ src/data.lua
@@ -8,4 +8,5 @@ require("prototypes.technologies")
 require("prototypes.tips-and-tricks")
 
 require("prototypes.compatibility.bobsep")
+require("prototypes.compatibility.space-exploration")
 require("prototypes.compatibility.SqueakThrough")
