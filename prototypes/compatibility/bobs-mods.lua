af0e1fa24e5096c6124697086ab87380d754bc9c
Gugarion <gugarion@gmail.com>
2025-06-06 03:24:47 -0700

    Add bob- prefixes to fix bob's mods compatibility (#41)

--- prototypes/compatibility/bobs-mods.lua
+++ prototypes/compatibility/bobs-mods.lua
@@ -3,43 +3,43 @@ if not (mods["bobelectronics"] and mods["bobplates"]) then
 end
 
 data.raw.recipe["duct-small"].ingredients = {
-  { type = "item", name = "silicon-nitride", amount = 4 },
-  { type = "item", name = "titanium-plate", amount = 1 },
+  { type = "item", name = "bob-silicon-nitride", amount = 4 },
+  { type = "item", name = "bob-titanium-plate", amount = 1 },
 }
 data.raw.recipe["duct-t-junction"].ingredients = {
   { type = "item", name = "duct-small", amount = 3 },
-  { type = "item", name = "silicon-nitride", amount = 3 },
-  { type = "item", name = "titanium-plate", amount = 1 },
+  { type = "item", name = "bob-silicon-nitride", amount = 3 },
+  { type = "item", name = "bob-titanium-plate", amount = 1 },
 }
 data.raw.recipe["duct-curve"].ingredients = {
   { type = "item", name = "duct-small", amount = 2 },
-  { type = "item", name = "silicon-nitride", amount = 2 },
-  { type = "item", name = "titanium-plate", amount = 1 },
+  { type = "item", name = "bob-silicon-nitride", amount = 2 },
+  { type = "item", name = "bob-titanium-plate", amount = 1 },
 }
 data.raw.recipe["duct-cross"].ingredients = {
   { type = "item", name = "duct-small", amount = 4 },
-  { type = "item", name = "silicon-nitride", amount = 4 },
-  { type = "item", name = "titanium-plate", amount = 1 },
+  { type = "item", name = "bob-silicon-nitride", amount = 4 },
+  { type = "item", name = "bob-titanium-plate", amount = 1 },
 }
 data.raw.recipe["duct-underground"].ingredients = {
   { type = "item", name = "duct-small", amount = 24 },
-  { type = "item", name = "silicon-nitride", amount = 12 },
-  { type = "item", name = "titanium-plate", amount = 3 },
+  { type = "item", name = "bob-silicon-nitride", amount = 12 },
+  { type = "item", name = "bob-titanium-plate", amount = 3 },
 }
 data.raw.recipe["non-return-duct"].ingredients = {
-  { type = "item", name = "silicon-nitride", amount = 24 },
-  { type = "item", name = "titanium-plate", amount = 12 },
+  { type = "item", name = "bob-silicon-nitride", amount = 24 },
+  { type = "item", name = "bob-titanium-plate", amount = 12 },
   { type = "item", name = "bob-pump-2", amount = 5 },
 }
 data.raw.recipe["duct-intake"].ingredients = {
-  { type = "item", name = "silicon-nitride", amount = 24 },
-  { type = "item", name = "titanium-plate", amount = 12 },
+  { type = "item", name = "bob-silicon-nitride", amount = 24 },
+  { type = "item", name = "bob-titanium-plate", amount = 12 },
   { type = "item", name = "bob-pump-2", amount = 10 },
   { type = "item", name = "advanced-circuit", amount = 5 },
 }
 data.raw.recipe["duct-exhaust"].ingredients = {
-  { type = "item", name = "silicon-nitride", amount = 24 },
-  { type = "item", name = "titanium-plate", amount = 12 },
+  { type = "item", name = "bob-silicon-nitride", amount = 24 },
+  { type = "item", name = "bob-titanium-plate", amount = 12 },
   { type = "item", name = "bob-pump-2", amount = 10 },
   { type = "item", name = "advanced-circuit", amount = 5 },
 }
